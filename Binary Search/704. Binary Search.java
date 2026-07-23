c55336593f05d38c622e6fb5274328f12cce382f
Durga <durga@example.com>
2026-07-23 12:02:20 +0530

    Add Binary Search solution and README

---
+++ Binary Search/704. Binary Search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length-1, m = 0;
+        while(l <= r){
+            m = l + (r-l) / 2;
+            if(nums[m] < target){
+                l = m + 1;
+            }else if(nums[m] > target){
+                r = m - 1;
+            }else{
+                return m;
+            }
+        }
+        return -1;
+    }
+}
